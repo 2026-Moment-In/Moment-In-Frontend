805df1e599a0f431f6ece7eedd6886bd451f9314
전희진 <hijeon80@gmail.com>
2026-05-20 12:55:34 +0900

    머지 충돌 해결 및 작업 내용 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -12,24 +11,9 @@ import AdminLivePage from "./pages/AdminLivePage";
 import QR from "./pages/QR";
 import ShowQR from "./pages/showQR";
 import QRview from './pages/QRview';
-=======
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import EditorPage from './pages/EditorPage';
-import QR from './pages/QR';
-import ShowQR from './pages/showQR';
-import QRview from './pages/QRview';
-import GalleryPage from './pages/GalleryPage';
-import GuestGalleryPage from './pages/guest/GalleryPage';
-import GuestPage from './pages/guest/MobileHomePage';
-import DashboardPage from './pages/DashboardPage';
-import WeddingPage from './pages/WeddingPage';
-import LiveScreenPage from './pages/LiveScreenPage';
->>>>>>> bc5569aacd5d294a207982e708aace1129f92cb3
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -51,28 +35,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-=======
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LandingPage />} />
-      <Route path="/enter" element={<GuestPage />} />
-      <Route path="/admin" element={<DashboardPage />} />
-      <Route path="/admin/create" element={<EditorPage />} />
-      <Route path="/admin/dashboard/:id" element={<DashboardPage />} />
-      <Route path="/editor" element={<EditorPage />} />
-      <Route path="/qr/:code" element={<QR />} />
-      <Route path="/show/:code" element={<ShowQR />} />
-      <Route path="/qrview/:code" element={<QRview />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/gallery/:code" element={<GalleryPage />} />
-      <Route path="/guest" element={<GuestPage />} />
-      <Route path="/guestgallery" element={<GuestGalleryPage />} />
-      <Route path="/guestgallery/:code" element={<GuestGalleryPage />} />
-      <Route path="/wedding/:code/*" element={<WeddingPage />} />
-      <Route path="/live/:code" element={<LiveScreenPage />} />
-    </Routes>
-  );
-}
-
-export default App;
->>>>>>> bc5569aacd5d294a207982e708aace1129f92cb3
